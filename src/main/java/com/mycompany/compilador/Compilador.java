bb3060dfb512afef1fd094f405852bebe359a077
Nicolas Andrei Ceruti <79486020+nicolas-ceruti@users.noreply.github.com>
2024-08-13 10:15:54 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/compilador/Compilador.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compilador;
+
+/**
+ *
+ * @author nicolasceruti
+ */
+public class Compilador {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
