4dc93ccde038366e3cc998357e2b77d452e9fe07
Nicolas Andrei Ceruti <79486020+nicolas-ceruti@users.noreply.github.com>
2024-09-09 21:27:44 -0300

    Lexico

---
+++ src/main/java/controller/SyntaticError.java
@@ -0,0 +1,16 @@
+package controller;
+
+import controller.AnalysisError;
+
+public class SyntaticError extends AnalysisError
+{
+    public SyntaticError(String msg, int position)
+	 {
+        super(msg, position);
+    }
+
+    public SyntaticError(String msg)
+    {
+        super(msg);
+    }
+}
--- src/main/java/view/Compiler.java
+++ src/main/java/view/Compiler.java
@@ -1,6 +1,10 @@
 package view;
 
 
+import controller.LexicalError;
+import controller.Lexico;
+import controller.Token;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
@@ -139,7 +143,33 @@ public class Compiler extends JFrame {
     }
 
     private void acaoBotaoCompilar() {
-        adicionarMensagem("compilação de programas ainda não foi implementada");
+        Lexico lexico = new Lexico();
+        lexico.setInput(editor.getText());
+        try {
+            Token t = null;
+            while ( (t = lexico.nextToken()) != null ) {
+                System.out.println(t.getLexeme());
+
+                // só escreve o lexema, necessário escrever t.getId, t.getPosition()
+
+                // t.getId () - retorna o identificador da classe. Olhar Constants.java e adaptar, pois
+                // deve ser apresentada a classe por extenso
+                // t.getPosition () - retorna a posição inicial do lexema no editor, necessário adaptar
+                // para mostrar a linha
+
+                // esse código apresenta os tokens enquanto não ocorrer erro
+                // no entanto, os tokens devem ser apresentados SÓ se não ocorrer erro, necessário adaptar
+                // para atender o que foi solicitado
+            }
+        }
+        catch ( LexicalError e ) {  // tratamento de erros
+            System.out.println(e.getMessage() + " em " + e.getPosition());
+
+            // e.getMessage() - retorna a mensagem de erro de SCANNER_ERRO (olhar ScannerConstants.java
+            // e adaptar conforme o enunciado da parte 2)
+            // e.getPosition() - retorna a posição inicial do erro, tem que adaptar para mostrar a
+            // linha
+        }
     }
 
     private void acaoBotaoNovo() {
