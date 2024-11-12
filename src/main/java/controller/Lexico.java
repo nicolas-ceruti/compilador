a246496b61913aa420cf1ba2b16804eb522281d9
Nicolas Andrei Ceruti <79486020+nicolas-ceruti@users.noreply.github.com>
2024-11-11 21:12:06 -0300

    Versao Final - Sintatico

--- src/main/java/controller/Lexico.java
+++ src/main/java/controller/Lexico.java
@@ -78,22 +78,8 @@ public class Lexico implements Constants
 
     private int nextState(char c, int state)
     {
-        int start = SCANNER_TABLE_INDEXES[state];
-        int end   = SCANNER_TABLE_INDEXES[state+1]-1;
-
-        while (start <= end)
-        {
-            int half = (start+end)/2;
-
-            if (SCANNER_TABLE[half][0] == c)
-                return SCANNER_TABLE[half][1];
-            else if (SCANNER_TABLE[half][0] < c)
-                start = half+1;
-            else  //(SCANNER_TABLE[half][0] > c)
-                end = half-1;
-        }
-
-        return -1;
+        int next = SCANNER_TABLE[state][c];
+        return next;
     }
 
     private int tokenForState(int state)
